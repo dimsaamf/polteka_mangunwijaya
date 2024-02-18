3b9f9c271c7993cc5226ad1ef77f9732ec738190
Dimsa Mutiara <06dimsa@gmail.com>
2024-02-18 22:09:03 +0700

    menambahkan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,46 +12,60 @@
         <!-- END: CSS Assets-->
     </head>
     <!-- END: Head -->
-    <body class="bg-biru160-polteka">
+    <body class="xl:bg-putih-polteka bg-biru160-polteka">
         <section>
-            <div class="px-32 grid grid-cols-2 gap-4">
+            <div class="block xl:grid px-20 grid-cols-2 gap-10">
                 <div class="hidden xl:flex flex-col min-h-screen">
                     <div class="my-auto">
-                        <img alt="Logo" class="w-1/2 -mt-16" src="illustration.svg">
-                        <div class="text-white font-polteka text-4xl leading-tight mt-10">
-                            Selamat Datang 
+                        <img alt="Logo" class="w-80 -mt-16" src="illustration.svg">
+                        <div class="text-putih-polteka font-polteka font-bold xl:text-3xl leading-tight mt-14">
+                            Sistem Informasi 
                             <br>
-                            Sistem Informasi Inventarisasi
+                            Inventarisasi Barang dan Peralatan
                         </div>
-                        <div class="mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Politeknik Katolik Mangunwijaya</div>
+                        <div class="mt-5 text-lg text-putih-polteka font-polteka text-opacity-70">Politeknik Katolik Mangunwijaya</div>
                     </div>
                 </div>
-                <div class="bg-putih-polteka min-h-screen flex flex-col">
-                    <img alt="Logo" class="w-[80px] mx-auto mt-20" src="logo.png">
-                    <div class="mt-10 ml-20">
-                        <h2 class="font-polteka font-bold text-biru160-polteka text-4xl text-left">
+                <div class="h-screen py-5 mx-auto">
+                    <img alt="Logo" class="w-[80px] mx-auto xl:mt-10" src="logo.png">
+                    <div class="mt-24">
+                        <img alt="Logo" class="xl:hidden w-[80px] mx-auto " src="logo.png">
+                        <h2 class="font-polteka font-bold text-biru160-polteka xl:text-3xl xl:text-left">
                             Masuk
                         </h2>
+                        <div class="xl:mt-2 xl:text-sm xl:mb-1 text-biru160-polteka font-polteka text-opacity-50">
+                            Masukkan email dan password Anda!
+                        </div>
+                        <hr class="border-1 border-biru160-polteka border-opacity-50 w-96">
                         <form method="POST" action="#">
                             @csrf
-                            <div class="intro-x mt-8">
-                                <input type="email" name="email" class="intro-x login__input py-3 px-4 block form-control @error('email') is-invalid @enderror" placeholder="Email" required autocomplete="email" autofocus>
-                                    @error('email')
+                            <div class="xl:mt-3">
+                                <div class="xl:mt-2 xl:text-sm xl:mb-1 text-biru160-polteka font-polteka">
+                                    Username*
+                                </div>
+                                <input type="username" name="username" class="xl:placeholder-sm placeholder-biru160-polteka placeholder-opacity-50 xl:text-sm text-biru160-polteka xl:py-2 xl:px-4 xl:w-96 xl:rounded-xl block form-control border border-biru160-polteka @error('username') is-invalid @enderror" placeholder="Username" required autocomplete="username" autofocus>
+                                    @error('username')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                <input type="password" id="password" name="password" class="intro-x login__input py-3 px-4 block mt-4 form-control @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="current-password">
+                                <div class="xl:mt-2 xl:text-sm xl:mb-1 text-biru160-polteka font-polteka">
+                                    Password*
+                                </div>
+                                <input type="password" id="password" name="password" class="xl:placeholder-sm placeholder-biru160-polteka placeholder-opacity-50 xl:text-sm text-biru160-polteka xl:py-2 xl:px-4 xl:w-96 xl:rounded-xl block form-control border border-biru160-polteka @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="current-password">
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                             </div>
-                            <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                                <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top" href="#" type="submit">{{ __('Login') }}</button>
+                            <div class="xl:mt-8 xl:text-left">
+                                <button class="xl:text-xs text-putih-polteka xl:font-bold text-polteka xl:rounded-xl xl:w-96 bg-biru160-polteka xl:py-2 xl:px-4 align-top" href="#" type="submit">Masuk</button>
                             </div>
                         </form>
+                    </div>
+                    <div class="xl:mt-32 xl:text-xs xl:mx-auto text-biru160-polteka font-polteka">
+                            © 2024 Tim Capstone 07 Teknik Komputer Universitas Diponegoro
                     </div>
                 </div>
             </div>
