029f412fb05d17a63b917f694599e22441135bf1
Dimsa Mutiara <06dimsa@gmail.com>
2024-03-05 21:39:02 +0700

    Responsive halaman ubah password

--- resources/views/rolewadir/contentwadir/dashboardwadiru.blade.php
+++ resources/views/rolewadir/contentwadir/dashboardwadiru.blade.php
@@ -8,7 +8,7 @@
             <div class="ml-2  text-hitam-polteka">Manajemen</div>
         </div> 
         <div class="hidden md:flex my-4 w-1/2 justify-start text-xs sm:text-md md:text-lg">
-            <div class="mr-2 text-merah180-polteka">Hei, Wadir</div>
+            <div class="mr-2 text-merah180-polteka">Hai, Wadir</div>
             <svg class="my-1.5 text-hitam-polteka" xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 20 20"><path fill="currentColor" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z"/></svg>
             <div class="ml-2  text-hitam-polteka">Manajemen</div>
         </div> 
