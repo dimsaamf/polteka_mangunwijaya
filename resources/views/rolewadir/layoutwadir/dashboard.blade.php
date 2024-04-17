1efa9309e160214aaacccff2eb8705f059c4b930
Dimsa Mutiara <06dimsa@gmail.com>
2024-04-17 21:56:55 +0700

    Menambahkan pop up profile

--- resources/views/rolewadir/layoutwadir/dashboard.blade.php
+++ resources/views/rolewadir/layoutwadir/dashboard.blade.php
@@ -254,7 +254,7 @@
         </div>
         <!-- Modal Notif -->
         <div id="modal" class="fixed z-10 inset-0 hidden">
-            <div class="flex mr-24 mt-24 justify-end">
+            <div class="flex mr-24 mt-40 md:mr-24 md:mt-24 justify-end">
                 <div class="bg-putih-polteka w-[285px] p-6 shadow-[rgba(0,0,15,0.5)_2px_2px_2px_0px] shadow-slate-300 rounded-lg">
                     <div class="w-full flex  text-black">
                         <div class="w-1/2 flex justify-start">
@@ -299,7 +299,7 @@
         </div>
         <!-- Modal Profile -->
         <div id="modal2" class="fixed z-10 inset-0 hidden">
-            <div class="flex mr-16 mt-24 justify-end">
+            <div class="flex mr-16 mt-40 md:mr-16 md:mt-24 justify-end">
                 <div class="bg-merah200-polteka w-[285px] p-6 shadow-[rgba(0,0,15,0.5)_2px_2px_2px_0px] shadow-slate-300 rounded-lg">
                     <div class="w-full flex  text-putih-polteka">
                         <div class="w-1/2 flex justify-start">
@@ -314,7 +314,15 @@
                             <h2 class="text-sm">wakil@gmail.com</h2>
                         </div>
                     </div>
-                    <hr class="mt-4 border-b border-abu-polteka w-full" />
+                    <hr class="mt-4 border-b-0 border-abu-polteka w-full" />
+                    <div class="w-full flex mt-4 text-putih-polteka">
+                        <div class="flex justify-start mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 1024 1024"><path fill="white" d="M868 732h-70.3c-4.8 0-9.3 2.1-12.3 5.8c-7 8.5-14.5 16.7-22.4 24.5a353.8 353.8 0 0 1-112.7 75.9A352.8 352.8 0 0 1 512.4 866c-47.9 0-94.3-9.4-137.9-27.8a353.8 353.8 0 0 1-112.7-75.9a353.3 353.3 0 0 1-76-112.5C167.3 606.2 158 559.9 158 512s9.4-94.2 27.8-137.8c17.8-42.1 43.4-80 76-112.5s70.5-58.1 112.7-75.9c43.6-18.4 90-27.8 137.9-27.8s94.3 9.3 137.9 27.8c42.2 17.8 80.1 43.4 112.7 75.9c7.9 7.9 15.3 16.1 22.4 24.5c3 3.7 7.6 5.8 12.3 5.8H868c6.3 0 10.2-7 6.7-12.3C798 160.5 663.8 81.6 511.3 82C271.7 82.6 79.6 277.1 82 516.4C84.4 751.9 276.2 942 512.4 942c152.1 0 285.7-78.8 362.3-197.7c3.4-5.3-.4-12.3-6.7-12.3m88.9-226.3L815 393.7c-5.3-4.2-13-.4-13 6.3v76H488c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h314v76c0 6.7 7.8 10.5 13 6.3l141.9-112a8 8 0 0 0 0-12.6"/></svg>
+                        </div>
+                        <div>
+                            <div class="text-md font-semibold">Logout</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
