6484fe0158bcada2f14ecc2379f5d666bbee85fe
Dimsa Mutiara <06dimsa@gmail.com>
2024-04-17 13:39:17 +0700

    Memperbaiki route dashboard

--- resources/views/rolewadir/layoutwadir/pengajuan.blade.php
+++ resources/views/rolewadir/layoutwadir/pengajuan.blade.php
@@ -23,7 +23,7 @@
             <hr class="border-1 w-full mt-3 border-putih-polteka border-opacity-25">
             <ul class="py-5 mx-8 mobileMenu hidden">
                 <li>
-                    <a href="{{ route('dashboard') }}" class="flex hover:font-bold">
+                    <a href="{{ route('dashboardwadir') }}" class="flex hover:font-bold">
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-7" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z"/></svg>
                         </div>
@@ -126,15 +126,15 @@
                 <div class="my-7"></div>
                 <ul>
                     <li>
-                        <a href="{{ route('dashboard') }}" class="hidden xl:justify-start xl:flex pt-2 hover:text-hitam-polteka hover:bg-abu-polteka hover:rounded-full hover:xl:ml-10 hover:lg:ml-10 hover:md:ml-6 w-full">
+                        <a href="{{ route('dashboardwadir') }}" class="hidden xl:justify-start xl:flex pt-2 hover:text-hitam-polteka hover:bg-abu-polteka hover:rounded-full hover:xl:ml-10 hover:lg:ml-10 hover:md:ml-6 w-full">
                             <div class="xl:ml-8 mb-3">
-                                <svg href="{{ route('dashboard') }}" xmlns="http://www.w3.org/2000/svg" class="xl:w-6 lg:w-8 md:w-8" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z"/></svg>
+                                <svg href="{{ route('dashboardwadir') }}" xmlns="http://www.w3.org/2000/svg" class="xl:w-6 lg:w-8 md:w-8" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z"/></svg>
                             </div>
                             <div class="ml-2 mb-3 hidden xl:block">Dashboard</div>
                         </a>
-                        <a href="{{ route('dashboard') }}" class="xl:hidden justify-center hover:justify-start flex hover:pt-2 hover:md:px-4 hover:lg:px-6 hover:text-hitam-polteka hover:bg-abu-polteka hover:rounded-full hover:md:ml-6 hover:lg:ml-10 hover:w-full">
+                        <a href="{{ route('dashboardwadir') }}" class="xl:hidden justify-center hover:justify-start flex hover:pt-2 hover:md:px-4 hover:lg:px-6 hover:text-hitam-polteka hover:bg-abu-polteka hover:rounded-full hover:md:ml-6 hover:lg:ml-10 hover:w-full">
                             <div class="xl:ml-8 mb-3">
-                            <svg href="{{ route('dashboard') }}" xmlns="http://www.w3.org/2000/svg" class="xl:w-6 lg:w-8 md:w-8" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z"/></svg>
+                            <svg href="{{ route('dashboardwadir') }}" xmlns="http://www.w3.org/2000/svg" class="xl:w-6 lg:w-8 md:w-8" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z"/></svg>
                             </div>
                         </a>
                     </li>
