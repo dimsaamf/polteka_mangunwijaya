d52838a9a0b62fad56012889e8f982dbdc2c2af2
Dimsa Mutiara <06dimsa@gmail.com>
2024-04-28 00:40:34 +0700

    Update service

--- app/Http/Controllers/KoorAdminLabFarmasi/DashboardKoorAdminLabFarmasiController.php
+++ app/Http/Controllers/KoorAdminLabFarmasi/DashboardKoorAdminLabFarmasiController.php
@@ -9,7 +9,8 @@ use Carbon\Carbon;
 
 class DashboardKoorAdminLabFarmasiController extends Controller
 {
-    public function index(Request $request)
+
+public function index(Request $request)
 {
     if ($request->has('sudah_dilayani')) {
         foreach ($request->sudah_dilayani as $notificationId) {
@@ -22,8 +23,8 @@ class DashboardKoorAdminLabFarmasiController extends Controller
     $notifications = InventarisLabFarmakognosi::where(function ($query) {
         $query->whereDate('tanggal_service', Carbon::today())
             ->orWhere(function ($query) {
-                $query->where('tanggal_service', '<', Carbon::today())
-                    ->whereRaw('DATE_ADD(tanggal_service, INTERVAL periode MONTH) = ?', [Carbon::today()]);
+                $query->whereRaw('DATE_ADD(tanggal_service, INTERVAL periode MONTH) >= ?', [Carbon::today()])
+                    ->where('tanggal_service', '<', Carbon::today());
             });
 
         $query->where('reminder', true);
@@ -31,16 +32,29 @@ class DashboardKoorAdminLabFarmasiController extends Controller
 
     // Perbarui tanggal layanan untuk setiap notifikasi yang memenuhi kriteria
     foreach ($notifications as $notification) {
-        $periode = $notification->periode;
-        $tanggalServiceTerbaru = $notification->tanggal_service->addMonths($periode);
-        $notification->tanggal_service = $tanggalServiceTerbaru;
-        $notification->save();
+        // Konversi string tanggal ke objek Carbon
+        $tanggalService = Carbon::parse($notification->tanggal_service);
+        
+        // Pastikan tanggal layanan valid sebelum melakukan operasi tambah bulan
+        if ($tanggalService->isValid()) {
+            $periode = $notification->periode;
+            $tanggalServiceTerbaru = $tanggalService->addMonths($periode);
+            
+            // Simpan tanggal layanan yang diperbarui kembali ke notifikasi
+            $notification->tanggal_service = $tanggalServiceTerbaru;
+            $notification->save();
+        } else {
+            // Lakukan penanganan kesalahan jika tanggal layanan tidak valid
+            // Misalnya, log pesan kesalahan atau kirim notifikasi ke administrator
+            Log::error('Tanggal layanan tidak valid untuk notifikasi dengan ID: ' . $notification->id);
+        }
     }
 
     $data['barangHabis'] = InventarisLabFarmakognosi::where('jumlah', '<', 20)->get();
     
     return view('rolekoorlabfarmasi.contentkoorlab.dashboard', compact('notifications', 'data'));
 }
+
 
 
     public function updateNotification(Request $request)
